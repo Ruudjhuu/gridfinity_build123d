5e3b570b35735425f755ab03fcddab9d71528029
Ruud Swinkels <ruudswinkels@hotmail.nl>
2023-11-11 20:58:46 +0100

    add examples

---
+++ examples/normal_bin.py
@@ -0,0 +1,27 @@
+from build123d import BuildPart
+from gridfinity_build123d import (
+    Base,
+    MagnetHole,
+    ScrewHole,
+    Bin,
+    Utils,
+    CompartmentsEqual,
+    Compartment,
+    Sweep,
+    Label,
+    StackingLip,
+    Direction,
+)
+
+with BuildPart() as part:
+    Base(grid_x=3, grid_y=1, features=[MagnetHole(), ScrewHole()])
+    Bin(
+        face=Utils.get_face_by_direction(Direction.TOP),
+        height=Utils.remaining_gridfinity_height(units=6),
+        compartments=CompartmentsEqual(
+            div_x=5, div_y=1, compartment_list=[Compartment(features=[Label(), Sweep()])] * 5
+        ),
+        lip=StackingLip(),
+    )
+
+part.part.export_stl("bin_3x1x6.stl")
---
+++ examples/simple_bin.py
@@ -0,0 +1,19 @@
+from build123d import BuildPart
+from gridfinity_build123d import (
+    Base,
+    Bin,
+    Utils,
+    CompartmentsEqual,
+    Compartment,
+    Direction,
+)
+
+with BuildPart() as part:
+    Base(grid_x=2, grid_y=1)
+    Bin(
+        face=Utils.get_face_by_direction(Direction.TOP),
+        height=Utils.remaining_gridfinity_height(units=3),
+        compartments=CompartmentsEqual(compartment_list=[Compartment()]),
+    )
+
+part.part.export_stl("bin_2x1x3.stl")
