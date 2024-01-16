8ec7c636008d460e1a0c2431e92cf340b43b4d57
Ruud Swinkels <ruudswinkels@hotmail.nl>
2024-01-16 18:48:33 +0100

    add gridfinityrefioned connector

---
+++ tests/test_unit_connectors.py
@@ -0,0 +1,13 @@
+from gridfinity_build123d.connectors import GridfinityRefinedConnector
+
+import testutils
+
+
+class GridfinityRefinedConnectorTest(testutils.UtilTestCase):
+    def test_gridfinityrefinedconnector(self) -> None:
+        part = GridfinityRefinedConnector()
+
+        bbox = part.bounding_box()
+
+        self.assertVectorAlmostEqual((13.6, 17.4, 2.8), bbox.size)
+        self.assertAlmostEqual(445.86058561576374, part.volume)
