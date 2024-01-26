2b0b504f73f44cae8596a155a449f15ab2cf1ba2
Ruud Swinkels <ruudswinkels@hotmail.nl>
2024-01-26 20:40:47 +0100

    Add bottom sides feature location

--- tests/test_unit_feature_locations.py
+++ tests/test_unit_feature_locations.py
@@ -1,16 +1,9 @@
 import testutils
-from build123d import (
-    Align,
-    Axis,
-    Box,
-    BuildPart,
-    CenterOf,
-    Mode,
-    Vector,
-)
+from build123d import Align, Axis, Box, BuildPart, CenterOf, Mode, Vector
 from gridfinity_build123d.feature_locations import (
     BottomCorners,
     BottomMiddle,
+    BottomSides,
     TopCorners,
     TopMiddle,
 )
@@ -123,3 +116,107 @@ class TopCornersTest(testutils.UtilTestCase):
         bbox = part.part.bounding_box()
         self.assertVectorAlmostEqual((50, 50, 30), bbox.size)
         self.assertAlmostEqual(50 * 50 * 30 - 4, part.part.volume)
+
+
+class BottomSidesTest(testutils.UtilTestCase):
+    def test_bottomsides(self) -> None:
+        with BuildPart() as part:
+            Box(50, 50, 30)
+
+            with BottomSides(offset=1).apply_to(
+                part.part,
+            ):
+                Box(
+                    1,
+                    2,
+                    3,
+                    align=(Align.CENTER, Align.MIN, Align.MAX),
+                    mode=Mode.SUBTRACT,
+                )
+
+        wires = part.faces().sort_by(Axis.Z)[0].inner_wires()
+
+        self.assertEqual(4, len(wires))
+        locations = [
+            Vector(23, 0, -15),
+            Vector(-23, 0, -15),
+            Vector(0, 23, -15),
+            Vector(0, -23, -15),
+        ]
+
+        for face in wires:
+            self.assertIn(face.center(CenterOf.BOUNDING_BOX), locations)
+
+        bbox = part.part.bounding_box()
+        self.assertVectorAlmostEqual((50, 50, 30), bbox.size)
+        self.assertAlmostEqual(50 * 50 * 30 - 4 * 3 * 2, part.part.volume)
+
+    def test_bottomsides_offset(self) -> None:
+        with BuildPart() as part:
+            Box(50, 50, 30)
+
+            with BottomSides(offset=2).apply_to(
+                part.part,
+            ):
+                Box(
+                    1,
+                    2,
+                    3,
+                    align=(Align.CENTER, Align.MIN, Align.MAX),
+                    mode=Mode.SUBTRACT,
+                )
+
+        wires = part.faces().sort_by(Axis.Z)[0].inner_wires()
+
+        self.assertEqual(4, len(wires))
+        locations = [
+            Vector(22, 0, -15),
+            Vector(-22, 0, -15),
+            Vector(0, 22, -15),
+            Vector(0, -22, -15),
+        ]
+
+        for face in wires:
+            self.assertIn(face.center(CenterOf.BOUNDING_BOX), locations)
+
+        bbox = part.part.bounding_box()
+        self.assertVectorAlmostEqual((50, 50, 30), bbox.size)
+        self.assertAlmostEqual(50 * 50 * 30 - 4 * 3 * 2, part.part.volume)
+
+    def test_bottomsides_multi_side(self) -> None:
+        with BuildPart() as part:
+            Box(50, 50, 30)
+
+            with BottomSides(nr_x=2, nr_y=3, offset=1).apply_to(
+                part.part,
+            ):
+                Box(
+                    1,
+                    2,
+                    3,
+                    align=(Align.CENTER, Align.MIN, Align.MAX),
+                    mode=Mode.SUBTRACT,
+                )
+
+        wires = part.faces().sort_by(Axis.Z)[0].inner_wires()
+
+        self.assertEqual(10, len(wires))
+        wire_locs = [
+            Vector(-23.0, -12.5, -15.0),
+            Vector(-(8 + 1 / 3), -23.0, -15.0),
+            Vector(8 + 1 / 3, -23.0, -15.0),
+            Vector(23.0, -12.5, -15.0),
+            Vector(-23.0, 0, -15.0),
+            Vector(-23.0, 12.5, -15.0),
+            Vector(-(8 + 1 / 3), 23.0, -15.0),
+            Vector(23.0, 0, -15.0),
+            Vector(8 + 1 / 3, 23.0, -15.0),
+            Vector(23.0, 12.5, -15.0),
+        ]
+
+        for wire in wires:
+            self.assertAlmostIn(wire.center(CenterOf.BOUNDING_BOX), wire_locs)
+
+        bbox = part.part.bounding_box()
+        self.assertVectorAlmostEqual((50, 50, 30), bbox.size)
+        self.assertAlmostEqual(50 * 50 * 30 - 10 * 3 * 2, part.part.volume)
